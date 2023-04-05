237a1ba657e436fcfb7e25c670ba1d2da213aa98
Norbaeocystin <baranrastislav@gmail.com>
2023-04-05 17:33:54 -0400

    search pools with mints

--- api.go
+++ api.go
@@ -2,8 +2,10 @@ package godium
 
 import (
 	"encoding/json"
+	"github.com/gagliardetto/solana-go"
 	"io/ioutil"
 	"net/http"
+	"sort"
 )
 
 // get pools data via raydium api
@@ -51,73 +53,117 @@ func GetTokens() (TokenList, error) {
 	return tokenData, err
 }
 
+type Pool struct {
+	ID            string `json:"id"`
+	MintA         string `json:"mintA"`
+	MintB         string `json:"mintB"`
+	VaultA        string `json:"vaultA"`
+	VaultB        string `json:"vaultB"`
+	MintDecimalsA int    `json:"mintDecimalsA"`
+	MintDecimalsB int    `json:"mintDecimalsB"`
+	AmmConfig     struct {
+		ID              string `json:"id"`
+		Index           int    `json:"index"`
+		ProtocolFeeRate int    `json:"protocolFeeRate"`
+		TradeFeeRate    int    `json:"tradeFeeRate"`
+		TickSpacing     int    `json:"tickSpacing"`
+		FundFeeRate     int    `json:"fundFeeRate"`
+		FundOwner       string `json:"fundOwner"`
+		Description     string `json:"description"`
+	} `json:"ammConfig"`
+	Tvl float64 `json:"tvl"`
+	Day struct {
+		Volume    float64 `json:"volume"`
+		VolumeFee float64 `json:"volumeFee"`
+		FeeA      float64 `json:"feeA"`
+		FeeB      float64 `json:"feeB"`
+		FeeApr    float64 `json:"feeApr"`
+		RewardApr struct {
+			A float64 `json:"A"`
+			B float64 `json:"B"`
+			C float64 `json:"C"`
+		} `json:"rewardApr"`
+		Apr      float64 `json:"apr"`
+		PriceMin float64 `json:"priceMin"`
+		PriceMax float64 `json:"priceMax"`
+	} `json:"day"`
+	Week struct {
+		Volume    float64 `json:"volume"`
+		VolumeFee float64 `json:"volumeFee"`
+		FeeA      float64 `json:"feeA"`
+		FeeB      float64 `json:"feeB"`
+		FeeApr    float64 `json:"feeApr"`
+		RewardApr struct {
+			A float64 `json:"A"`
+			B float64 `json:"B"`
+			C float64 `json:"C"`
+		} `json:"rewardApr"`
+		Apr      float64 `json:"apr"`
+		PriceMin float64 `json:"priceMin"`
+		PriceMax float64 `json:"priceMax"`
+	} `json:"week"`
+	Month struct {
+		Volume    float64 `json:"volume"`
+		VolumeFee float64 `json:"volumeFee"`
+		FeeA      float64 `json:"feeA"`
+		FeeB      float64 `json:"feeB"`
+		FeeApr    float64 `json:"feeApr"`
+		RewardApr struct {
+			A float64 `json:"A"`
+			B float64 `json:"B"`
+			C float64 `json:"C"`
+		} `json:"rewardApr"`
+		Apr      float64 `json:"apr"`
+		PriceMin float64 `json:"priceMin"`
+		PriceMax float64 `json:"priceMax"`
+	} `json:"month"`
+	LookupTableAccount string `json:"lookupTableAccount"`
+}
+
 type PoolsV3Data struct {
-	Data []struct {
-		ID            string `json:"id"`
-		MintA         string `json:"mintA"`
-		MintB         string `json:"mintB"`
-		VaultA        string `json:"vaultA"`
-		VaultB        string `json:"vaultB"`
-		MintDecimalsA int    `json:"mintDecimalsA"`
-		MintDecimalsB int    `json:"mintDecimalsB"`
-		AmmConfig     struct {
-			ID              string `json:"id"`
-			Index           int    `json:"index"`
-			ProtocolFeeRate int    `json:"protocolFeeRate"`
-			TradeFeeRate    int    `json:"tradeFeeRate"`
-			TickSpacing     int    `json:"tickSpacing"`
-			FundFeeRate     int    `json:"fundFeeRate"`
-			FundOwner       string `json:"fundOwner"`
-			Description     string `json:"description"`
-		} `json:"ammConfig"`
-		Tvl float64 `json:"tvl"`
-		Day struct {
-			Volume    float64 `json:"volume"`
-			VolumeFee float64 `json:"volumeFee"`
-			FeeA      float64 `json:"feeA"`
-			FeeB      float64 `json:"feeB"`
-			FeeApr    float64 `json:"feeApr"`
-			RewardApr struct {
-				A float64 `json:"A"`
-				B float64 `json:"B"`
-				C float64 `json:"C"`
-			} `json:"rewardApr"`
-			Apr      float64 `json:"apr"`
-			PriceMin float64 `json:"priceMin"`
-			PriceMax float64 `json:"priceMax"`
-		} `json:"day"`
-		Week struct {
-			Volume    float64 `json:"volume"`
-			VolumeFee float64 `json:"volumeFee"`
-			FeeA      float64 `json:"feeA"`
-			FeeB      float64 `json:"feeB"`
-			FeeApr    float64 `json:"feeApr"`
-			RewardApr struct {
-				A float64 `json:"A"`
-				B float64 `json:"B"`
-				C float64 `json:"C"`
-			} `json:"rewardApr"`
-			Apr      float64 `json:"apr"`
-			PriceMin float64 `json:"priceMin"`
-			PriceMax float64 `json:"priceMax"`
-		} `json:"week"`
-		Month struct {
-			Volume    float64 `json:"volume"`
-			VolumeFee float64 `json:"volumeFee"`
-			FeeA      float64 `json:"feeA"`
-			FeeB      float64 `json:"feeB"`
-			FeeApr    float64 `json:"feeApr"`
-			RewardApr struct {
-				A float64 `json:"A"`
-				B float64 `json:"B"`
-				C float64 `json:"C"`
-			} `json:"rewardApr"`
-			Apr      float64 `json:"apr"`
-			PriceMin float64 `json:"priceMin"`
-			PriceMax float64 `json:"priceMax"`
-		} `json:"month"`
-		LookupTableAccount string `json:"lookupTableAccount"`
-	} `json:"data"`
+	Data []Pool `json:"data"`
+}
+
+func (p Pool) HasMintKey(mint solana.PublicKey) bool {
+	if p.MintA == mint.String() || p.MintB == mint.String() {
+		return true
+	}
+	return false
+}
+
+func (p Pool) HasMintKeyAsString(mint string) bool {
+	if p.MintA == mint || p.MintB == mint {
+		return true
+	}
+	return false
+}
+
+// find pools with mints
+func (p PoolsV3Data) GetPoolsViaMints(mintA, mintB solana.PublicKey) []Pool {
+	found := make([]Pool, 0)
+	for _, pool := range p.Data {
+		if pool.HasMintKey(mintA) && pool.HasMintKey(mintB) {
+			found = append(found, pool)
+		}
+	}
+	sort.SliceStable(found, func(i, j int) bool {
+		return int(found[i].Tvl) > int(found[j].Tvl)
+	})
+	return found
+}
+
+// find pools with symbola symbolb
+func (p PoolsV3Data) GetPoolsViaMintsAsStrins(mintA, mintB string) []Pool {
+	found := make([]Pool, 0)
+	for _, pool := range p.Data {
+		if pool.HasMintKeyAsString(mintA) && pool.HasMintKeyAsString(mintB) {
+			found = append(found, pool)
+		}
+	}
+	sort.SliceStable(found, func(i, j int) bool {
+		return int(found[i].Tvl) > int(found[j].Tvl)
+	})
+	return found
 }
 
 type Token struct {
